af8e72cee6cc4c51a869d663a4ef392b0cb61d89
Chris Grimmett <chris@grimtech.net>
2014-12-08 23:21:06 -0800

    server socket emissions begun

--- server/client/js/game4.js
+++ server/client/js/game4.js
@@ -1,13 +1,37 @@
+
+
+
 var socket = io.connect('http://' + document.domain + ':' + location.port);
 
+socket.on('join', function(player) {
+   console.log('player ' + player.id + ' joined with ' + player.cursor + '. msg: ' + player.msg + ' ' + player.nextRound);
+});
+
+
 var Q = Quintus()
-        .include("Sprites, Scenes, Input, 2D, Touch")
+        .include("Sprites, Scenes, Input, 2D, Touch, UI")
         .setup({width: 1024, height: 640, scaleToFit: true})
         .controls().touch()
-        
+
 Q.input.mouseControls();
 Q.gravityY = 0;
 Q.gravityX = 0;
+
+Q.Sprite.extend("GhostPlayer", {
+   init: function(p) {
+       this._super(p, {
+           sheet: "ghost",
+           cx: 0,
+           cy: 0,
+           y: 1000
+       });
+    },
+
+    step: function(dt) {
+        this.p.x = Q.inputs['mouseX'];
+        this.p.y = Q.inputs['mouseY'];        
+    }
+});
 
 Q.MovingSprite.extend("Player",{
     init: function(p) {
@@ -18,29 +42,24 @@ Q.MovingSprite.extend("Player",{
             cx: 0,
             cy: 0,
             type: Q.SPRITE_FRIENDLY,
-            
-            moveTargetX: 512,
-            moveTargetY: 512,
-            moveTargetReached: false,
-            moveInProgress: false,
-            moveStepsAllowed: 3,
-            moveStepsElapsed: 0
-            
+            collisionMask: Q.SPRITE_ENEMY,
+
         });
         this.add('mouseControls, 2d');
 
         this.on("hit.sprite",function(collision) {
+            //console.dir(collision.obj);
             if(collision.obj.isA("Tower")) {
                 Q.stageScene("endGame",1, { label: "You Won!" }); 
                 this.destroy();
             }
-            else if (collision.ob.isA("Wall")) {
-                Q.stageScene("endGame",1, { label: "You touched a wall and died!" });
+            else if (collision.obj.isA("Spike")) {
+                Q.stageScene("endGame",1, { label: "You got hit by spikes" });
                 this.destroy();            
             }
         });
     },
-    
+
     step: function(dt) {
         this.p.x = Q.inputs['mouseX'];
         this.p.y = Q.inputs['mouseY'];
@@ -57,12 +76,28 @@ Q.Sprite.extend("Button", {
     init: function(p) {
         this._super(p, { 
             sheet: 'tiles',
-            type: Q.SPRITE_UI
+            type: Q.SPRITE_UI,
+            cx:16,
+            cy:16
         });
-        
+
         this.on("touch", function(t) {
-            console.log('button touched.');
-            //console.dir(t);
+            //console.log('button touched.');
+            var buttonFrame = t.obj.p.frame;
+
+            Q.select("Trap", Q.activeStage.i).items.forEach(function(trap) {
+                if (trap.p.frame == buttonFrame) {
+                    Q.stage(0).insert(new Q.Spike({
+                        y: trap.p.y + 16,
+                        x: trap.p.x + 16,
+                        //frame: t.obj.p.frame // match frame
+                    }));
+
+                    trap.destroy();
+                    t.obj.destroy();
+                    // @todo socket delete button & trap
+                }
+            });
         });
     }
 });
@@ -71,11 +106,10 @@ Q.Sprite.extend("Trap", {
    init: function(p) {
        this._super(p, {
            sheet: 'tiles',
-           type: Q.SPRITE_UI
-       });
-       
-       this.on("touch", function(t) {
-           console.log('u dead');
+           type: Q.SPRITE_UI,
+           cx: 0,
+           cy: 0,
+           z: 100
        });
    }
 });
@@ -83,9 +117,42 @@ Q.Sprite.extend("Trap", {
 Q.Sprite.extend("Spike", {
     init: function(p) {
         this._super(p, {
-            sheet: 'tiles',
-            type: Q.SPRITE_ENEMY
+            sheet: 'spikes',
+            type: Q.SPRITE_ENEMY,
+            scale: 4,
+            z: -10,
+
+            lifeSpan: 0.5,
+            age: 0,
+            born: 0
         });
+
+        this.on("hit", this, function(collision) {
+            console.log('collision');
+            if(collision.obj.isA("Player")) { 
+                Q.stageScene("endGame",1, { label: "You Died" }); 
+                collision.obj.destroy();
+            }
+        });
+    },
+
+    step: function(dt) {
+        // live and enjoy being a spike for a short time, then die.
+        if (this.p.born) {
+            if (this.p.age >= this.p.lifeSpan) {
+                // kill if life over
+                this.destroy();
+            }
+            else {
+                // age and rotate
+                this.p.age = Q.stage(0).time - this.p.born > this.p.lifeSpan
+                this.p.angle += 500 * dt;
+            }
+        }
+        else {
+            // get birthed
+            this.p.born = Q.stage(0).time;
+        }
     }
 });
 
@@ -94,14 +161,14 @@ Q.Sprite.extend("Enemy",{
   init: function(p) {
     this._super(p, { sheet: 'enemy', vx: 100 });
     this.add('2d, aiBounce');
-    
+
     this.on("bump.left,bump.right,bump.bottom",function(collision) {
       if(collision.obj.isA("Player")) { 
-        Q.stageScene("endGame",1, { label: "You Died" }); 
+        Q.stageScene("endGame",1, { label: "You Died" });
         collision.obj.destroy();
       }
     });
-    
+
     this.on("bump.top",function(collision) {
       if(collision.obj.isA("Player")) { 
         this.destroy();
@@ -112,49 +179,56 @@ Q.Sprite.extend("Enemy",{
 });
 
 Q.scene("level1",function(stage) {
-    
+
+    // get map from server
     var border = stage.collisionLayer(new Q.TileLayer({ 
         dataAsset: 'level.json',
         sheet: 'tiles' }));
     //border.p.y -= 9;
     //border.p.x += 41;
-    
+
     var trapButton1 = stage.insert(new Q.Button({
-        x: 32+16, 
-        y: 32+16,
+        x: 32*1+16, 
+        y: 32*1+16,
         frame: 10,
     }));
-    
+
     var trapButton2 = stage.insert(new Q.Button({
         x: 32+16,
         y: 32*4+16,
         frame: 11,
     }));
-    
+
     var trapButton3 = stage.insert(new Q.Button({
         x: 32+16,
         y: 32*8+16,
         frame: 12
     }));
-    
+
     var trapButton4 = stage.insert(new Q.Button({
         x: 32+16,
         y: 32*13+16,
         frame: 13
     }));
-    
+
     var trapButton5 = stage.insert(new Q.Button({
         x: 32+16,
         y: 32*17+16,
         frame: 14
     }));
-    
+
     var flag = stage.insert(new Q.Button({
         x: 32*8+16,
         y: 32*8+16,
         frame: 15
     }));
-    
+
+    var trap1 = stage.insert(new Q.Trap({
+        x: 32*10+16,
+        y: 32*10+16,
+        frame: 10
+    }));
+
     var player = stage.insert(new Q.Player({
         x: 32*30+16,
         y: 32*5+16
@@ -166,10 +240,13 @@ Q.scene("level1",function(stage) {
 });
 
 Q.scene('endGame',function(stage) {
+    var player = stage.insert(new Q.GhostPlayer());
+
+
   var box = stage.insert(new Q.UI.Container({
     x: Q.width/2, y: Q.height/2, fill: "rgba(0,0,0,0.5)"
   }));
-  
+
   var button = box.insert(new Q.UI.Button({ x: 0, y: 0, fill: "#CCCCCC",
                                            label: "Play Again" }))         
   var label = box.insert(new Q.UI.Text({x:10, y: -10 - button.p.h, 
@@ -185,4 +262,9 @@ Q.load("cursors.png, cursors.json, level.json, tiles.png", function() {
   Q.sheet("tiles","tiles.png", { tilew: 32, tileh: 32 });
   Q.compileSheets("cursors.png","cursors.json");
   Q.stageScene("level1");
+});
+
+
+document.addEventListener('beforeunload', function() {
+   socket.close(); 
 });
--- server/index.js
+++ server/index.js
@@ -3,8 +3,8 @@ var http = require('http');
 var app = express();
 var server = http.Server(app);
 var io = require('socket.io')(server);
-
-
+var moment = require('moment');
+var maze = require('./maze');
 
 // Serve the game html css js
 // ==========================
@@ -15,21 +15,32 @@ app.use(express.static(__dirname + '/client'));
 
 server.listen(port);
 console.log('server listening on port ' + port);
+console.log('status page at localhost:' + port + '/status');
 
-
+app.get('/status', function(req, res) {
+    res.send(players);
+});
 
 
 // Handle the game multiplayer
 // ===========================
 
+// some vars
+var availableCursors = [0, 1, 2, 3, 5, 6];
 var players = {};
+var roundLength = moment({seconds: 10});
+
+
+
+// some functions
+var getRandomInt = function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 var getUID = function getUID() {
     // thanks to http://stackoverflow.com/a/1349426/1004931
-
     var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     // get a unique string
-
     var provenUnique = false;
     while (!provenUnique) {
 	var id = "";
@@ -41,68 +52,106 @@ var getUID = function getUID() {
     return id;
 }
 
+var getCursor = function getCursor() {
+    return availableCursors[Math.floor(Math.random() * (availableCursors.length + 1))];
+}
+
+
+var round = {
+    startTime: null,
+    next: function nextRound() {
+        // only 1 player is in game, no next round possible.
+        if (players.length === 0) {
+            return null;
+        }
+
+        // 2 players are in game, so next game in 3s
+        else if (players.length === 1) {
+            return moment().add(3, 'seconds')
+        }
+
+        // game is already in progress, so next game is when this game is over
+        else {
+            return moment(this.startTime).add(roundLength);
+        }
+    },
+    start: function startRound(socket) {
+        var level = maze.generate();
+        this.startTime = moment();
+        socket.emit('start', {
+            level: level,
+            boss: getRandomInt(0, players.length),
+            time: this.startTime
+        });
+    }
+}
+
+
+// socket
 
 
 io.on('connection', function(socket) {
 
-    // {
-    // asdfsd: {id: 3, team: 0},
-    
-    // },
-      
-    
+    // new player joined
+    // assign a unique ID
     var player = {};
     player['id'] = getUID();
+    player['cursor'] = getCursor();
 
-    // pick team for player
-    // pick the opposite team from the last player who joined
-    // or if there are no players in the game right now, choose at random
 
-    if (players.length > 0) {
-	// other people are in game
-	// get the last team assignment and choose the opposite
-	if (players[players.length - 1].team == 0) {
-	        player['team'] = 1;
-	        
-	    } else {
-		    player['team'] = 0;
-		}
-	
-    } else {
-	// nobody is in game, choose a random team
-	player['team'] = Math.floor(Math.random() * 2);
+    // if this is the only player in the game
+    if (players.length === 0) {
+        socket.emit('join', {
+            msg: '1 more player needed to start',
+            id: player.id,
+            cursor: player.cursor
+        });
     }
-
-
+    
+    // there is 1 player in the game
+    // start game in 3 seconds
+    else if (players.length === 1) {
+        socket.emit('join', {
+            msg: 'GET READY!',
+            cursor: player.cursor,
+            id: player.id,
+            nextRound: round.next()
+        });
+    }
+    
+    // there is more than 1 player already in game
+    else {
+        socket.emit('join', {
+            msg: 'next round starts in: ',
+            id: player.id,
+            cursor: player.cursor,
+            nextRound: round.next()
+        });
+    }
+    
     // add this player to array holding current players
-    console.log('new player ' + player.id + ' added to team ' + player.team);
-    players[player.id] = { id: player.id, team: player.team };
+    console.log('new player ' + player.id + ' added');
+    players[player.id] = {
+        id: player.id,
+        cursor: player.cursor
+    };
     console.dir(players);
 
-    
-    
-    socket.emit('info', { id: player.id,
-			    team: player.team,
-			  });
 
-    socket.on('spawn', function(p) {
-	console.log('player spawned.');
-	socket.broadcast.emit('spawn', { id: p.id,
-					  team: p.team
-					        });
-    });
-    
+    // emit movement updates when player sends them
     socket.on('move', function(p) {
-	player['x'] = p.x;
-	player['y'] = p.y;
-	
-	socket.broadcast.emit('move', { id: p.id,
-					   x: p.x,
-					   y: p.y
-					 });
+        player['x'] = p.x;
+        player['y'] = p.y;
+
+        socket.broadcast.emit('move', {
+            id: p.id,
+            x: p.x,
+            y: p.y
+        });
     });
     
-
+    round.start(socket);
+    
     // player disconnect
     socket.on('disconnect', function(p) {
 	console.log('player ' + player.id + ' disconnect');
